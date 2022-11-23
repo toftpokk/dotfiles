04f2d1091169aafe2d53c33b904cdd924e434733
kong-wep <pakkapol.20100@gmail.com>
2022-11-23 22:43:33 +0700

    Added vim-plug installation

--- install.sh
+++ install.sh
@@ -66,4 +66,9 @@ ln -sf $DOTS/scripts $LOCAL/
 [ ! -d "$LOCAL/share" ] && mkdir "$LOCAL/share"
 ln -sf $DOTS/fonts $LOCAL/share/
 
+# Vim plug
+echo "Installing Vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 echo "Done!"
