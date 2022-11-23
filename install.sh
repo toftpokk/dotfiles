35ed2d67408b814edc5dc01cac3f92c857510e19
kong-wep <pakkapol.20100@gmail.com>
2022-11-23 21:35:42 +0700

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 # inspired by https://github.com/PoSayDone/.dotfiles_new
-CONFIG="$HOME/dotfiles/config"
-CONFIG_DIR="$HOME/.config"
+set -euo pipefail
+DOTS="$HOME/dotfiles"
+CONFIG="$HOME/.config"
+LOCAL="$HOME/.local"
 
 # Paru
 echo "Installing paru"
+sudo pacman -S --noconfirm --needed 'rust' 'base-devel'
 git clone https://aur.archlinux.org/paru.git
-cd paru
+pushd paru
 makepkg -si
+popd
 
 # Dependencies
 echo "Installing dependencies"
-pacman -Syu --needed \
+sudo pacman -Syu --noconfirm --needed \
 'neovim' 'alacritty' 'sxhkd' 'bspwm' 'git' 'xorg' 'xorg-xinit' 'lf' 'pipewire' \
-'pipewire-jack' 'wireplumber' 'noto-fonts' 'noto-fonts-cjk' 'fonts-tlwg' \
+'pipewire-jack' 'wireplumber' 'noto-fonts' 'noto-fonts-cjk' \
 'firefox' 'zathura' 'zathura-cb' 'zathura-pdf-mupdf' 'dunst' \
 'fcitx5' 'fcitx5-configtool' 'fcitx5-mozc'  'wget' \
 'highlight' 'picom' 'rofi' 'playerctl' 'wmctrl' \
 'xdotool' 'rsync' 'fzf' 'sshfs' 'udisks2' 'ueberzug' 'unzip' \
-'xwallpaper' 'cmus' 'openvpn' 'zenity' 'wine' 'winetricks' 'base-devel'
+'xwallpaper' 'cmus' 'openvpn'
+# 'zenity' 'wine' 'winetricks'
 
-paru -S 'nsxiv'
+paru -S --needed --noconfirm 'nsxiv' 'fonts-tlwg'
+
 
 # Clone dots
 echo "Cloning dotfiles"
@@ -28,30 +34,36 @@ git clone https://github.com/kong-wep/dotfiles.git
 
 # Symlinks
 echo "Symlinking"
-ln -sf $DIR/config/alacritty $CONFIG_DIR/alacritty
-ln -sf $DIR/config/bspwm $CONFIG_DIR/bspwm
-ln -sf $DIR/config/cmus $CONFIG_DIR/cmus
-ln -sf $DIR/config/dunst $CONFIG_DIR/dunst
-ln -sf $DIR/config/eww $CONFIG_DIR/eww
-ln -sf $DIR/config/fcitx5 $CONFIG_DIR/fcitx5
-ln -sf $DIR/config/gtk-3.0 $CONFIG_DIR/gtk-3.0
-ln -sf $DIR/config/highlight $CONFIG_DIR/highlight
-ln -sf $DIR/config/lf $CONFIG_DIR/lf
-ln -sf $DIR/config/nvim $CONFIG_DIR/nvim
-ln -sf $DIR/config/picom $CONFIG_DIR/picom
-ln -sf $DIR/config/rofi $CONFIG_DIR/rofi
-ln -sf $DIR/config/sxhkd $CONFIG_DIR/zathura
-ln -sf $DIR/config/commands $CONFIG_DIR/commands
-ln -sf $DIR/config/locations $CONFIG_DIR/locations
-ln -sf $DIR/config/mimeapps.list $CONFIG_DIR/mimeapps.list
+[ ! -d "$CONFIG" ] && mkdir "$CONFIG"
+ln -sf $DOTS/config/alacritty $CONFIG/
+ln -sf $DOTS/config/bspwm $CONFIG/
+ln -sf $DOTS/config/cmus $CONFIG/
+ln -sf $DOTS/config/dunst $CONFIG/
+ln -sf $DOTS/config/eww $CONFIG/
+ln -sf $DOTS/config/fcitx5 $CONFIG/
+ln -sf $DOTS/config/gtk-3.0 $CONFIG/
+ln -sf $DOTS/config/highlight $CONFIG/
+ln -sf $DOTS/config/lf $CONFIG/
+ln -sf $DOTS/config/nvim $CONFIG/
+ln -sf $DOTS/config/picom $CONFIG/
+ln -sf $DOTS/config/rofi $CONFIG/
+ln -sf $DOTS/config/sxhkd $CONFIG/
+ln -sf $DOTS/config/zathura $CONFIG/
+ln -sf $DOTS/config/commands $CONFIG/
+ln -sf $DOTS/config/locations $CONFIG/
+ln -sf $DOTS/config/mimeapps.list $CONFIG/
 
-ln -sf $DIR/home/bash_profile $HOME/.bash_profile
-ln -sf $DIR/home/bashrc $HOME/.bashrc
-ln -sf $DIR/home/gtkrc-2.0 $HOME/.gtkrc-2.0
-ln -sf $DIR/home/xinputrc $HOME/.xinputrc
-ln -sf $DIR/home/xinitrc $HOME/.xinitrc
-ln -sf $DIR/home/Xresources $HOME/.Xresources
+ln -sf $DOTS/home/bash_profile $HOME/.bash_profile
+ln -sf $DOTS/home/bashrc $HOME/.bashrc
+ln -sf $DOTS/home/gtkrc-2.0 $HOME/.gtkrc-2.0
+ln -sf $DOTS/home/inputrc $HOME/.inputrc
+ln -sf $DOTS/home/xinitrc $HOME/.xinitrc
+ln -sf $DOTS/home/Xresources $HOME/.Xresources
 
-ln -sf $DIR/scripts $HOME/.local/scripts
+[ ! -d "$LOCAL" ] && mkdir "$LOCAL"
+ln -sf $DOTS/scripts $LOCAL/
+
+[ ! -d "$LOCAL/share" ] && mkdir "$LOCAL/share"
+ln -sf $DOTS/fonts $LOCAL/share/
 
 echo "Done!"
