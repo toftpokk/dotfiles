3e3fef1e89ffc1f98e6c225bfd5db155418e4c13
toftpokk <pakkapol.20100@gmail.com>
2024-02-26 14:29:01 +0700

    Remove nvim transparency

--- config/nvim/lua/user/theme.lua
+++ config/nvim/lua/user/theme.lua
@@ -8,14 +8,14 @@ local function hi(hl_group,options)
 end
 
 local highlights ={
-	["LineNr"]={ guifg="#787878", guibg="NONE", ctermfg="240", ctermbg="0"},
+	["LineNr"]={ guifg="#787878",guibg="#383838", ctermfg="240", ctermbg="0"},
 	["CursorLineNr"]={ gui="bold", guifg="#b8b8b8", guibg="#282828", ctermfg="250", ctermbg="235"},
 	["Visual"]={ guibg="#383838", ctermbg="240" },
 	["SignColumn"]     ={ guifg="#585858", guibg="NONE", ctermfg="240", ctermbg="0"},
 	["GitGutterAdd"]   ={ guibg="NONE", ctermbg="0"},
 	["GitGutterChange"]={ guibg="NONE", ctermbg="0"},
 	["GitGutterDelete"]={ guibg="NONE", ctermbg="0"},
-	["Normal"]={ guifg="#d8d8d8", guibg="NONE", ctermfg="15", ctermbg="0"},
+	["Normal"]={ guifg="#d8d8d8", guibg="#282828", ctermfg="15", ctermbg="0"},
 	["User1"]={ guifg="#181818", guibg="#5066C6", ctermfg="15", ctermbg="237"},
 	["User2"]={ guifg="#f8f8f8", guibg="#383838", ctermfg="15", ctermbg="237"},
 	["User3"]={ guifg="#70CCAD", guibg="#383838", ctermfg="79", ctermbg="237"},
