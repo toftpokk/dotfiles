9ea452df259f8ec5bf666902d3b2579fe9aa2bd5
toftpokk <pakkapol.20100@gmail.com>
2024-05-25 22:19:50 +0700

    Update userjs

--- config/mozilla/user.js
+++ config/mozilla/user.js
@@ -1,5 +1,13 @@
 // My userjs
-// middle click paste
-user_pref("middlemouse.paste",false);
-user_pref("signon.rememberSignons",false);
-user_pref("browser.contentblocking.category","strict");
+user_pref("middlemouse.paste",false); // middle click paste
+user_pref("signon.rememberSignons",false); // no remember passwords
+user_pref("browser.contentblocking.category","strict"); // strict content blocking 
+// Sponsored Shortcuts, src:arkenfox
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.pinned", "[]");
+// Pocket
+user_pref("extensions.pocket.enabled", false);
+// Data collection
+user_pref("browser.discovery.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
