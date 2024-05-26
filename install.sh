283e0270fdbf5c547c6d521cb64d32bf2b618623
toftpokk <pakkapol.20100@gmail.com>
2024-05-26 14:41:52 +0700

    Fix gopath

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ CONFIG="$HOME/.config"
 LOCAL="$HOME/.local"
 GIT_PROJECTS="$HOME/git-projects"
 STATE="$HOME/.local/state"
+# Fix bug: leftover gopath in $HOME
+GOPATH="$CONFIG/go"
 
 # Pacman
 echo "Installing Pacman config"
