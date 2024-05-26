48bc7588793f41c1937b2d3e198434a3e0145545
toftpokk <pakkapol.20100@gmail.com>
2024-05-26 11:35:49 +0700

    Wine installation

--- install.sh
+++ install.sh
@@ -51,10 +51,8 @@ sudo pacman -Syu --noconfirm --needed \
 'acpilight' 'man-db' 'imagemagick' 'ghostscript' \
 'fuse2' 'xclip' 'deluge-gtk' \
 'ldns' 'python-virtualenv' 'bash-language-server' 'pyright' 'python-pip' \
-'docker' 'docker-compose' 'pavucontrol' 'typescript-language-server' 'wofi'
-
-echo "To Install manually"
-echo 'wine' 'winetricks'
+'docker' 'docker-compose' 'pavucontrol' 'typescript-language-server' 'wofi' \
+'wine'
 
 # for screenshare
 # xdg-desktop-portal-hyprland https://gist.github.com/PowerBall253/2dea6ddf6974ba4e5d26c3139ffb7580
